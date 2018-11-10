e9f8a14ed8f1bf23e684b31d77016521cbbc6635
acple <silentsphere110@gmail.com>
2018-11-10 23:20:32 +0900

    support multiple input

--- Program.cs
+++ Program.cs
@@ -1,25 +1,32 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BccksConverter
 {
-    class Program
+    internal class Program
     {
-        static async Task Main(string[] args)
+        private static async Task Main(string[] args)
         {
             if (args.Length == 0)
                 throw new Exception("no parameter");
+            if (!args.All(File.Exists))
+                throw new Exception("invalid arguments");
 
-            var sourcePath = args[0];
-            var fullPath = Path.GetFullPath(sourcePath);
-            var dir = Path.GetDirectoryName(fullPath);
-            var fileName = Path.GetFileNameWithoutExtension(fullPath);
-            var ext = Path.GetExtension(fullPath);
+            var tasks = args.Select(Path.GetFullPath).Distinct().Select(x => Run(x));
+            await Task.WhenAll(tasks).ConfigureAwait(false);
+        }
+
+        private static async Task Run(string inputPath)
+        {
+            var dir = Path.GetDirectoryName(inputPath);
+            var fileName = Path.GetFileNameWithoutExtension(inputPath);
+            var ext = Path.GetExtension(inputPath);
             var outputPath = Path.Combine(dir, $"{fileName}_converted{ext}");
 
-            using (var input = new FileStream(sourcePath, FileMode.Open))
+            using (var input = new FileStream(inputPath, FileMode.Open))
             using (var reader = new StreamReader(input, Encoding.UTF8))
             {
                 var from = await reader.ReadToEndAsync().ConfigureAwait(false);
